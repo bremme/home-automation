1aefe4e0b763d84a35e62baca96c232a6b85c463
Bram Harmsen <bramharmsen@gmail.com>
2014-09-13 00:01:59 +0200

    refactored states and templates

--- client/js/app.js
+++ client/js/app.js
@@ -13,28 +13,59 @@ angular.module( 'app', [
 
     $urlRouterProvider.otherwise( '/' );
 
-    $stateProvider
-      .state( 'home', {
-        url: '/',
-        views: {
-          '': {
-            templateUrl: 'partials/widgets'
-          },
-          'switches-widget@home': {
-            templateUrl: 'partials/switches-widget',
-            controller: 'switchesCtrl'
-          },
-          'climate-widget@home': {
-            templateUrl: 'partials/climate-widget',
-            controller: 'climateCtrl'
-          }
-        }
-      })
-    .state( 'test', {
-      url: '/test',
-      templateUrl: 'test',
-      controller: 'testCtrl'
+
+    $stateProvider.state('root', {
+      url: '',
+      abstract: true,
+      views: {
+        'navbar@' : { templateUrl: 'partials/navbar'},
+        'alerts@' : { 
+          templateUrl: 'partials/alerts',
+          controller : 'alertsCtrl'
+        },
+        'footer@'  : { templateUrl: 'partials/footer'}
+      }
     })
+    .state('root.home', {
+      url: '/',
+      views: {
+        'switches-widget@': {
+          templateUrl: 'partials/home-switches-widget',
+          controller: 'switchesCtrl'
+        },
+        'climate-widget@': {
+          templateUrl: 'partials/home-climate-widget',
+          controller: 'climateCtrl'
+        }
+      }
+    })    
+
+
+    // $stateProvider
+    //   .state( 'home', {
+    //     url: '/',
+    //     views: {
+    //       '': {
+    //         templateUrl: 'partials/widgets'
+    //       },
+    //       'switches-widget@home': {
+    //         templateUrl: 'partials/switches-widget',
+    //         controller: 'switchesCtrl'
+    //       },
+    //       'climate-widget@home': {
+    //         templateUrl: 'partials/climate-widget',
+    //         controller: 'climateCtrl'
+    //       },
+    //       'alerts@home': {
+    //         templateUrl
+    //       }
+    //     }
+    //   })
+    // .state( 'test', {
+    //   url: '/test',
+    //   templateUrl: 'test',
+    //   controller: 'testCtrl'
+    // })
   }]);
 
 
--- client/js/controllers/alertsCtrl.js
+++ client/js/controllers/alertsCtrl.js
@@ -2,10 +2,12 @@
 
 angular.module('app.controllers')
 // Alert controller //////////////////////////////////////////////////////////
-.controller('alertCtrl', [ '$scope'], function( $scope ) {
+.controller('alertsCtrl', [ '$scope' , function( $scope ) {
 
   $scope.alerts = [
-    { type: 'info', msd: 'Yeah boy!'}
+    { type: 'info', msg: 'Yeah boy!'}
   ];
 
-});
+
+
+}]);
